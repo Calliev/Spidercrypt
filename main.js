cc290e1af298b3f3237c9bd7fd2ed0959526c880
Vanessa Oliveira <vanessa.abmkt@gmail.com>
2022-05-18 21:16:06 -0300

    código 4 do site

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-/=============== SHOW MENU ===============/
+/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToogle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-    
 /* ====== MENU SHOW ======*/
 
 if(navToogle){
@@ -12,7 +11,7 @@ if(navToogle){
     })
 }
 
-/* ====== MENU CLOSE ====== */
+/*====== MENU CLOSE =====*/
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
@@ -58,3 +57,33 @@ let swiperPopular = new Swiper(".popular__container", {
       },
 
  });
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
